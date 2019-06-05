3936b54d54955b3dc9069ada6b682d73c2ec90ac
KvS <virajkunwar98@gmail.com>
2019-06-06 00:17:40 +0530

    delete feature

--- app/src/main/java/com/example/notesapp/EditNoteActivity.java
+++ app/src/main/java/com/example/notesapp/EditNoteActivity.java
@@ -1,6 +1,8 @@
 package com.example.notesapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,20 +10,30 @@ import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.HashSet;
+
 import static com.example.notesapp.MainActivity.arrayAdapter;
 import static com.example.notesapp.MainActivity.notes;
 
 public class EditNoteActivity extends AppCompatActivity {
+    int noteId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
         EditText editText = (EditText) findViewById(R.id.editText);
         Intent intent = getIntent();
-        final int noteId = intent.getIntExtra("noteId",-1);
+        noteId = intent.getIntExtra("noteId",-1);
         if(noteId != -1){
             editText.setText(notes.get(noteId));
         }
+        else{
+                MainActivity.notes.add("");
+                noteId = MainActivity.notes.size()-1;
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+        }
+
         editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -32,6 +44,10 @@ public class EditNoteActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 MainActivity.notes.set(noteId, String.valueOf(charSequence));
                 MainActivity.arrayAdapter.notifyDataSetChanged();
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notesapp", Context.MODE_PRIVATE);
+                HashSet<String> hashSet = new HashSet<>(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes",hashSet).apply();
             }
 
             @Override
--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.notesapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +17,7 @@ import android.widget.ListView;
 
 import java.sql.Struct;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
@@ -43,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = (ListView) findViewById(R.id.listView);
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notesapp", Context.MODE_PRIVATE);
+        HashSet<String> hashSet = (HashSet<String>) sharedPreferences.getStringSet("notes",null);
+        if(hashSet==null){
+                notes.add("Example Note");
+        }else {
+                notes = new ArrayList<>(hashSet);
+        }
+
         notes.add("Example Note");
         arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,notes);
         listView.setAdapter(arrayAdapter);
@@ -54,6 +68,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
 
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final int j = i;
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.alert_dark_frame)
+                        .setTitle("Are you sure")
+                        .setMessage("Do you really want to delete this??")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                notes.remove(j);
+                                arrayAdapter.notifyDataSetChanged();
+
+                                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notesapp", Context.MODE_PRIVATE);
+                                HashSet<String> hashSet = new HashSet<>(MainActivity.notes);
+                                sharedPreferences.edit().putStringSet("notes",hashSet).apply();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+                return true;
+            }
         });
     }
 }
